b4ce5b01349d00553818e72aaf3ba0b9ff6a31ef
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2021-02-18 08:45:49 +0700

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,6 +1,6 @@
 module.exports = ({
     name: "play",
     aliases: ['p'],
-    code: `$playSong[$message;:x: Cannot find song or error]
+    code: `$description[Playing - \`$playSong[$message;:x: Cannot find song or error]\`]
 $onlyIf[$voiceID!=;You need to be in a voice channel!]`
 })
